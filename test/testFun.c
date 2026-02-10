97e87a0d7819fd2a371d94141c90e9b8a21a5338
lokeshbabusarode <67642763+lokeshbabusarode@users.noreply.github.com>
2026-02-10 19:02:24 +0530

    Update testFun.c
    Added Test function for sec_memset_c() 
    
    Signed-off-by: lokeshbabusarode <67642763+lokeshbabusarode@users.noreply.github.com>

--- test/testFun.c
+++ test/testFun.c
@@ -3,6 +3,14 @@
 
 int main()
 {
+	char buf[20];
+
+	/* sec_memset_s() test */
+    if (sec_memset_s(buf, sizeof(buf), 'A', 10) == SEC_OK)
+        printf("memset success\n");
+    else
+        printf("memset failed\n");
+	
 	printf("--- %d\n", testFun(100));
 
 	return(0);
